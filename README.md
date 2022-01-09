e3bd18816b6fdc2a7c3ac1082b5d9eb2705143fd
oaken <oakenf@gmail.com>
2022-01-09 22:56:41 +0000

    Fixes to background generator (works, but resoloution is incorrect) and addition of Ghostscript requirement.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # SoupProject
 
-A project that aims to take different reference images to create soup cans.
+_The can as it currently is, is subject to change (including the introduction of completely randomly colored can bodies)._
+
+A Project that aims to take different reference images to create soup cans.
 
 Personal Development aims for working with images within Python.
+
+Currently, **Ghostscript** is required to use this. This is most likely going to stay this way.
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from PIL import Image
+from PIL import Image, EpsImagePlugin
 from turtle import Screen, Turtle
 import random
-import background
-from background import topcolor, bottomcolor, screen, height, width
+
+# import background
+# from background import topcolor, bottomcolor, screen, height, width
 
 #  use my jank-ass method of creating a gradient background
 
 #  create a gradient image using turtle
+#  find 6 (3 + 3) random values for the rgb of 2 colors
 addition = 0
 colorlist = []
 for i in range(256):
@@ -25,15 +27,15 @@ print("Turtle has Started!")
 topcolor = (colorchoices[0] / 255, colorchoices[1] / 255, colorchoices[2] / 255)
 bottomcolor = (colorchoices[3] / 255, colorchoices[4] / 255, colorchoices[5] / 255)
 screen = Screen()
+#  set up turtle and run it
+turt = Turtle()
+Screen().setup(1080, 1080)
 height = screen.window_height()
 width = screen.window_width()
-
-
 deltas = [(hue - topcolor[index]) / height for index, hue in enumerate(bottomcolor)]
 
-turt = Turtle()
 turt.color(topcolor)
-turt.speed(0)
+turt.speed(0)  # this is as fast as it will go ;-;
 turt.penup()
 turt.goto(-width / 2, height / 2)
 turt.pendown()
@@ -42,24 +44,24 @@ direction = 1
 
 for distance, y in enumerate(range(height // 2, -height // 2, -1)):
     turt.forward(width * direction)
-    turt.color([topcolor[i] + delta * distance for i, delta in enumerate(deltas)])
+    turt.color([topcolor[i] + delta * distance for i, delta in enumerate(deltas)])  # this gives me a warning but works
     turt.sety(y)
 
     direction *= -1
 
 turt.getscreen().getcanvas().postscript(file="temp/background.ps")
+screen.bye()
 print("Turtle Finished!")
 
 #  make the gradient image usable
+EpsImagePlugin.gs_windows_binary = r'C:\Program Files\gs\gs9.55.0\bin\gswin64c'  # Ghostscript path goes here
 preconvertbg = Image.open("temp/background.ps")
 preconvertbg.save("temp/background_convert.png")
-
 
 #  create a list of all can colors
 cans = ["ref/can/black.png", "ref/can/blue.png", "ref/can/green.png", "ref/can/orange.png", "ref/can/pink.png",
         "ref/can/purple.png", "ref/can/red.png", "ref/can/teal.png", "ref/can/white.png", "ref/can/yellow.png"]
 canchoice = random.choice(cans)
-
 
 #  set the background, pick a can color, and layer all correctly
 bg = Image.open("temp/background_convert.png")
